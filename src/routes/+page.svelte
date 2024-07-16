fa826802a1d9640d77f267defe0227eb8fdea44e
Bence Boér <bence0822@gmail.com>
2024-07-16 21:30:53 +0200

    Set up Supabase login and shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
 	import type { Tables } from '../types/supabase';
+	import { supabase } from '../supabaseClient';
+	import type { PostgrestSingleResponse } from '@supabase/supabase-js';
 
-	export let data: { units: Tables<'units'>[] };
+	let state: PostgrestSingleResponse<Tables<'units'>[]>;
+	supabase
+		.from('units')
+		.select('*')
+		.then((response) => {
+			if (response.error) throw response.error;
+			state = response;
+		});
+
+	$: console.log(state);
 </script>
 
 <ul>
-	{#each data.units as unit}
+	{#each state?.data ?? [] as unit}
 		<li>{unit.name}</li>
 	{/each}
 </ul>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import { supabase } from '../../supabaseClient';
 
+	let email: string;
+	let password: string;
+
+	const login = () => {
+		supabase.auth
+			.signInWithPassword({
+				email: email,
+				password: password
+			})
+			.then(console.log);
+	};
 </script>
 
 <form>
-    <input type="text">
+	<input type="email" name="email" id="email" bind:value={email} />
+	<input type="password" name="password" id="password" bind:value={password} />
+	<Button variant="outline" on:click={login}>Login</Button>
 </form>
